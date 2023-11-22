2ac24fc59a5fd0e879f63b915f0d2d916140e657
B4nanaJuice <griesmaxime2@gmail.com>
2023-11-22 20:40:53 +0100

    Added function test
    + Test to know how exposed fuctions work

--- test_qt.py
+++ test_qt.py
@@ -7,7 +7,14 @@ eel.init('web')
 def say_hello_py(x):
     print('Hello from %s' % x)
 
+@eel.expose
+def test(text):
+    print("creted element")
+
 say_hello_py('Python World!')
 eel.say_hello_js('Python World!')   # Call a Javascript function
+
+test("coucou")
+eel.test("Hey toi")
 
 eel.start('hello.html', size=(300, 200), mode = "firefox")  # Start
