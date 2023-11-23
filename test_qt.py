bb6bc876cbc10bae99c11e059d0a08bd37664d0e
B4nanaJuice <griesmaxime2@gmail.com>
2023-11-23 18:46:25 +0100

    Update test_qt.py
    + Added new function to get current date to return to JavaScript side
    
    ~ Put all unused functions to comment
    ~ Renamed the file to open

--- test_qt.py
+++ test_qt.py
@@ -5,24 +5,29 @@ import subprocess
 # Set web files folder
 eel.init('web')
 
-@eel.expose                         # Expose this function to Javascript
-def say_hello_py(x):
-    print('Hello from %s' % x)
+# @eel.expose                         # Expose this function to Javascript
+# def say_hello_py(x):
+#     print('Hello from %s' % x)
+
+# @eel.expose
+# def test(text):
+#     print("creted element")
+
+# say_hello_py('Python World!')
+# eel.say_hello_js('Python World!')   # Call a Javascript function
+
+# test("coucou")
+# eel.test("Hey toi")
+
+# @eel.expose
+# def request(): # Send the response to the javascript side
+#     resp = subprocess.Popen(f"$HOME/file_sync_gui/fsync init -o A -d B", stdout=subprocess.PIPE,shell=True, text=True).stdout.read()
+#     eel.response(json.loads(resp))
+#     print(resp)
 
 @eel.expose
-def test(text):
-    print("creted element")
+def getDate():
+    resp = subprocess.Popen(f"date", stdout=subprocess.PIPE,shell=True, text=True).stdout.read()
+    eel.refresh_date(resp)
 
-say_hello_py('Python World!')
-eel.say_hello_js('Python World!')   # Call a Javascript function
-
-test("coucou")
-eel.test("Hey toi")
-
-@eel.expose
-def request(): # Send the response to the javascript side
-    resp = subprocess.Popen(f"$HOME/file_sync_gui/fsync init -o A -d B", stdout=subprocess.PIPE,shell=True, text=True).stdout.read()
-    eel.response(json.loads(resp))
-    print(resp)
-
-eel.start('hello.html', size=(300, 200), mode = "firefox")  # Start
+eel.start('index.html', size=(300, 200), mode = "firefox")  # Start
