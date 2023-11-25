936afb9f28b4303bbddeaf4960da25305ef64e7d
B4nanaJuice <griesmaxime2@gmail.com>
2023-11-25 11:38:25 +0100

    Added first interaction with fsync
    + Added function to initialize the two directories
    
    ~ To reduce size of lines, imported subprocess as sp
    
    - Removed commented code that wouldn't be used again

--- test_qt.py
+++ test_qt.py
@@ -1,33 +1,19 @@
 import eel
 import json
-import subprocess
+import subprocess as sp
 
 # Set web files folder
 eel.init('web')
 
-# @eel.expose                         # Expose this function to Javascript
-# def say_hello_py(x):
-#     print('Hello from %s' % x)
-
-# @eel.expose
-# def test(text):
-#     print("creted element")
-
-# say_hello_py('Python World!')
-# eel.say_hello_js('Python World!')   # Call a Javascript function
-
-# test("coucou")
-# eel.test("Hey toi")
-
-# @eel.expose
-# def request(): # Send the response to the javascript side
-#     resp = subprocess.Popen(f"$HOME/file_sync_gui/fsync init -o A -d B", stdout=subprocess.PIPE,shell=True, text=True).stdout.read()
-#     eel.response(json.loads(resp))
-#     print(resp)
-
 @eel.expose
 def getDate():
-    resp = subprocess.Popen(f"date", stdout=subprocess.PIPE,shell=True, text=True).stdout.read()
+    resp = sp.Popen(f"date", stdout=sp.PIPE,shell=True, text=True).stdout.read()
     eel.refresh_date(resp)
+
+@eel.expose
+def initialize(__origin: str, __destination: str):
+    resp = sp.Popen(f"$HOME/file_sync_gui/fsync init -o {__origin} -d {__destination}", stdout=sp.PIPE,shell=True, text=True).stdout.read()
+    print(resp)
+    eel.response(json.loads(resp))
 
 eel.start('index.html', size=(300, 200), mode = "firefox")  # Start
