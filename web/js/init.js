a927f17bb9a55755da80fbb12aadb9cbcc5c115e
B4nanaJuice <griesmaxime2@gmail.com>
2023-11-26 14:38:41 +0100

    Renamed init.js to commands.js as all commands will be handled in this file

--- web/js/init.js
+++
@@ -1,5 +0,0 @@
-function init() {
-    let __origin = document.querySelector('.init_content').querySelectorAll('.directory')[0].value
-    let __destination = document.querySelector('.init_content').querySelectorAll('.directory')[1].value
-    eel.initialize(__origin, __destination)
-}
